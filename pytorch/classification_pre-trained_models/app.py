6300f6c43249693c987da8dc036ab2a4e66c914b
Tapasya Gutta <tapasya234@gmail.com>
2025-10-16 16:02:28 +0530

    Implement the code to use pretrained models for image classification.

--- pytorch/classification_pre-trained_models/app.py
+++ pytorch/classification_pre-trained_models/app.py
@@ -1,13 +1,62 @@
+from matplotlib import axis
 import numpy as np
-import requests
-import torchvision
 import torch
-import zipfile
 import matplotlib.pyplot as plt
-import glob as glob
-import os
-import ast
 from PIL import Image
-from torchvision.models import mobilenet_v3_small, MobileNet_V3_Small_Weights
+
+import torchvision.models as pretrainedModels
 
 from data_path import DATA_PATH
+
+
+def loadModel():
+    # model = pretrainedModels.efficientnet_b7(weights="IMAGENET1K_V1")
+    model = pretrainedModels.mobilenet_v3_small(weights="IMAGENET1K_V1")
+    model.eval()
+    return model
+
+
+def loadClasses(fileName):
+    with open(DATA_PATH + fileName) as f:
+        whip = {en: line.strip() for en, line in enumerate(f.readlines())}
+
+    return whip
+
+
+def loadImage(imageName: str):
+    img = Image.open(DATA_PATH + imageName)
+
+    # get evaluation metrics
+    # weights = pretrainedModels.EfficientNet_B7_Weights.IMAGENET1K_V1
+    weights = pretrainedModels.MobileNet_V3_Small_Weights.IMAGENET1K_V1
+    preprocess = weights.transforms()
+
+    # Preprocess the image and add it to a batch.
+    imgPreprocessed = preprocess(img)
+    return torch.unsqueeze(imgPreprocessed, axis=0)
+
+
+def generatePredictions(input, model, classes, topK=5):
+    predictions = model(input)
+    predictions = torch.softmax(predictions, -1)
+    sortedPredictions = np.argsort(predictions.detach().numpy())[0, -topK:][::-1]
+
+    print("\nPredictions:")
+    for prediction in sortedPredictions:
+        print(
+            f"Class: {classes[prediction].split(',')[0]} Score: {predictions[0][prediction]*100:0.2f}%"
+        )
+
+    return predictions
+
+
+img = loadImage("jellyfish.jpg")
+print("Shape: ", img.shape)
+print("Data type: ", img.dtype)
+print("Min pixel value: ", img.min())
+print("Max pixel value: ", img.max())
+
+model = loadModel()
+classes = loadClasses("imagenet_classes.txt")
+
+generatePredictions(img, model, classes)
--- pytorch/classification_pre-trained_models/data_path.py
+++ pytorch/classification_pre-trained_models/data_path.py
@@ -2,5 +2,5 @@ import os
 
 DATA_PATH = os.path.join(
     os.getcwd(),
-    "python3/deep-learning-projects/pytorch/classification_pre-trained_models/",
+    "pytorch/classification_pre-trained_models/",
 )
